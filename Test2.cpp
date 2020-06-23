9f96d243103600df06dc4387f665a6096ac2f39a
XuYangYu233 <523191489@qq.com>
2020-06-23 15:11:46 +0800

    again

--- Test2.cpp
+++ Test2.cpp
@@ -4,7 +4,7 @@
 #include"fun.c"
 //int fun(int n);
 //int a = 3;wtf??????
-//int b = 3;aaaa?????wtf try again
+//int b = 3;aaaa?????wtf try again and again
 int main(){
     printf("%d\n",fun(10));
     Sleep(1777.776);
