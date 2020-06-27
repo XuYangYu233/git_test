46f4a90d3c4fd49753eb029ac2d2406c30cd4dae
XuYangYu233 <523191489@qq.com>
2020-06-27 15:00:10 +0800

    try style

--- Test9.cpp
+++ Test9.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
-#include<cstring>
+#include <cstring>
+#include <iostream>
 
-char duibi(char *origin, char *after){
-    char temp=0,sum=0;
+char duibi(char* origin, char* after)
+{
+    char temp = 0, sum = 0;
     sum = origin[0];
-    for(int i=1;i<strlen(origin);i++){
+    for (int i = 1; i < strlen(origin); i++) {
         temp = origin[i];
         sum = sum ^ temp;
     }
-    for(int i=0;i<strlen(after);i++){
+    for (int i = 0; i < strlen(after); i++) {
         temp = after[i];
         sum = sum ^ temp;
     }
@@ -16,12 +17,13 @@ char duibi(char *origin, char *after){
     return sum;
 }
 
-int main(){
-    char origin[]="23456789QJKA";
-    char after[]="2346789QJKA";
+int main()
+{
+    char origin[] = "23456789QJKA";
+    char after[] = "2346789QJKA";
     char temp;
     temp = duibi(origin, after);
-    printf("%c",temp);
+    printf("%c", temp);
 
     return 0;
 }
